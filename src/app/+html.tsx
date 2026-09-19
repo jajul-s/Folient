f6842a430fa0d3d6d258a014e97b71de4f7a4821
Folient <folient@local>
2026-09-19 12:00:32 +0200

    feat: add Penny Stock unlock animation and web app updates

---
+++ src/app/+html.tsx
@@ -0,0 +1,25 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#14213D" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="manifest" href="/manifest.json" />
+        <script
+          defer
+          data-website-id={process.env.EXPO_PUBLIC_UMAMI_WEBSITE_ID}
+          src="https://cloud.umami.is/script.js"
+        />
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
